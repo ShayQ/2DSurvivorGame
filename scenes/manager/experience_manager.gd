ce82c993b4ca4a6bbebaac777bdb6b80d917c345
Shay Quinn <shayquinn100@gmail.com>
2023-10-18 20:02:27 +0200

    added vial pickup animation

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -3,11 +3,11 @@ extends Node
 signal experience_updated(current_experience: float, target_experience: float)
 signal level_up(new_level: int)
 
-const TARGET_EXPERIENCE_GROWTH = 1
+const TARGET_EXPERIENCE_GROWTH = 0
 
 var current_experience = 0
 var current_level = 1
-var target_experience = 1
+var target_experience = 3
 
 
 func _ready():
