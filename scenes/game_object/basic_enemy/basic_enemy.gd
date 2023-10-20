73c9211ffe189173e64525afb539790e43ac4daa
Shay Quinn <shayquinn100@gmail.com>
2023-10-20 23:41:50 +0200

    hit sound effects

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 @onready var visuals = $Visuals
 @onready var velocity_component = $VelocityComponent
 
+
+func _ready():
+	$HurtboxComponent.hit.connect(on_hit)
+
 func _process(delta: float) -> void:
 	velocity_component.accelerate_to_player()
 	velocity_component.move(self)
@@ -10,6 +14,10 @@ func _process(delta: float) -> void:
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(-move_sign, 1)
+
+
+func on_hit():
+	$HitRandomAudioPlayerComponent.play_random()
 
 #func get_direction_to_player():
 #	var player_node = get_tree().get_first_node_in_group("player") as Node2D
